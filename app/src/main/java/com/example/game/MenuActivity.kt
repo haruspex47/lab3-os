dbec6fcacd75793f4592cd83c70ee575a213c149
kl <haruspex47>
2023-05-16 19:30:27 +0300

    добавил мультипликацию

--- app/src/main/java/com/example/game/MenuActivity.kt
+++ app/src/main/java/com/example/game/MenuActivity.kt
@@ -1,16 +1,23 @@
 package com.example.game
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+
 
 class MenuActivity : AppCompatActivity() {
+
+    private var animationView: LottieAnimationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        animationView = findViewById(R.id.animationView)
+        animationView?.setAnimation(R.raw.tumbnail)
+        animationView?.playAnimation()
 
         val playButton: Button = findViewById(R.id.playButton)
         playButton.setOnClickListener {
